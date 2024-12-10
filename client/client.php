368c77b3ca48b04c893db3bc9953b5f7735c1dc0
Neil Casas <neilalfonzcasas@gmail.com>
2024-12-10 23:44:59 +0800

    updated sql script

--- client/client.php
+++ client/client.php
@@ -12,6 +12,13 @@
     die('Connect Error(' . $mysqli->connect_errno . ')' . $mysqli->connect_error);     
   }
   
+  $accounts_query = "SELECT * FROM account WHERE client_id = $client_id";
+  $accounts_result = $mysqli->query($accounts_query);
+  $accounts = $accounts_result->fetch_all(MYSQLI_ASSOC);
+
+  $loans_query = "SELECT * FROM loan WHERE client_id = $client_id";
+  $loans_result = $mysqli->query($loans_query);
+  $loans = $loans_result->fetch_all(MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -24,5 +31,38 @@
   <body>
     <h1>Client</h1>
     <p>Your client id is <?php echo $_SESSION["client_id"]; ?></p>
+
+    <h2>Accounts</h2>
+    <table>
+      <tr>
+        <th>Account ID</th>
+        <th>Account Type</th>
+        <th>Balance</th>
+      </tr>
+      <?php foreach ($accounts as $account) { ?>
+        <tr>
+          <td><?php echo $account["acct_id"]; ?></td>
+          <td><?php echo $account["acct_type"]; ?></td>
+          <td><?php echo $account["acct_balance"]; ?></td>
+        </tr>
+      <?php } ?>
+    </table>
+    <button>Request Account Creation</button>
+    <h2>Loans</h2>
+    <table>
+      <tr>
+        <th>Loan ID</th>
+        <th>Loan Type</th>
+        <th>Loan Amount</th>
+      </tr>
+      <?php foreach ($loans as $loan) { ?>
+        <tr>
+          <td><?php echo $loan["loan_id"]; ?></td>
+          <td><?php echo $loan["loan_type"]; ?></td>
+          <td><?php echo $loan["loan_amount"]; ?></td>
+        </tr>
+      <?php } ?>
+    </table>
+    <button>Request Loan</button>
   </body>
 </html>
